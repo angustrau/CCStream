088de7bce1d2fe527499323fa00b01d8568fa1d9
Angus Trau <angus.amtra5@gmail.com>
2015-12-19 14:21:49 +1100

    Added url parameter processing

--- CC-Remote-Display/app.js
+++ CC-Remote-Display/app.js
@@ -1,1 +1,29 @@
-﻿console.log('Hello world');
+﻿const http = require("http");
+
+function handleHTTPRequest(request, response) {
+    //Process the url
+    var requestParts = request.url.split("?");
+    var requestURL = requestParts[0];
+    var requestParameters = {};
+    if (requestParts[1]) {
+        requestParts[1].split("&").forEach(function(v) {
+            var kvPair = v.split("=");
+            requestParameters[kvPair[0]] = kvPair[1];
+        });
+    }
+
+    switch (requestURL) {
+        case "/poll":
+            
+            break;
+        case "/push":
+            break;
+        default:
+            break;
+    }
+    response.end(JSON.stringify(requestParameters));
+    //response.end(requestURL);
+}
+
+var server = http.createServer(handleHTTPRequest);
+server.listen(5678, function() {console.log("Listener started on port 5678")});
