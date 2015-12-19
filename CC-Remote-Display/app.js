496f02be1c8c9bbe4f97996f1c37749747704b6b
Angus Trau <angus.amtra5@gmail.com>
2015-12-19 16:47:58 +1100

    Moved to express, implemented socket.io, added parameter checks

--- CC-Remote-Display/app.js
+++ CC-Remote-Display/app.js
@@ -1,29 +1,117 @@
-﻿const http = require("http");
+﻿var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
-function handleHTTPRequest(request, response) {
-    //Process the url
-    var requestParts = request.url.split("?");
-    var requestURL = requestParts[0];
-    var requestParameters = {};
-    if (requestParts[1]) {
-        requestParts[1].split("&").forEach(function(v) {
-            var kvPair = v.split("=");
-            requestParameters[kvPair[0]] = kvPair[1];
-        });
+app.get("/", function(req, res) {
+    console.log("Client requested '/'");
+
+    res.sendFile(__dirname + "/html/index.html");
+})
+
+app.get("/pull", function(req, res) {
+    res.send("Temp");
+})
+
+app.get("/push", function(req, res) {
+    if (!req.query.id) {
+        res.status(400).send("No ID");
+        return;
+    }
+    
+    if (!req.query.op) {
+        res.status(400).send("No operation");
+        return;
     }
 
-    switch (requestURL) {
-        case "/poll":
-            
+    //Check for valid parameters
+    switch(req.query.op) {
+        case "write":
+            if (!req.query.param || typeof(req.query.param) != "string") {
+                res.status(400).send("Invalid parameter to 'write'");
+                return;    
+            }
+
             break;
-        case "/push":
+        case "blit":
+            if (!req.query.param || typeof(req.query.param) != "object" || !req.query.param[0] || !req.query.param[1] || !req.query.param[2] || req.query.param[0].length != req.query.param[1].length || req.query.param[0].length != req.query.param[2].length) {
+                res.status(400).send("Invalid parameter to 'blit'");
+                return;    
+            }
+
+            break;
+        case "clear":
+            break;
+        case "clearLine":
+            break;
+        case "setCursorPos":
+            if (!req.query.param || typeof(req.query.param) != "object" || !req.query.param[0] || !req.query.param[1]) {
+                res.status(400).send("Invalid parameter to 'setCursorPos'");
+                return;    
+            }
+
+            break;
+        case "setCursorBlink":
+            if (!req.query.param || typeof(req.query.param) != "object" || !req.query.param[0] || !req.query.param[1] || !(req.query.param[0] == "true" || req.query.param[0] == "false") || !(req.query.param[1] == "true" || req.query.param[1] == "false")) {
+                res.status(400).send("Invalid parameter to 'setCursorBlink'");
+                return;    
+            }
+
+            break;
+        case "scroll":
+            if (!req.query.param || typeof(req.query.param) != "string") {
+                res.status(400).send("Invalid parameter to 'scroll'");
+                return;    
+            }
+
+            break;
+        case "setTextColor":
+            if (!req.query.param || typeof(req.query.param) != "string") {
+                res.status(400).send("Invalid parameter to 'setTextColor'");
+                return;    
+            }
+
+            break;
+        case "setBackgroundColor":
+            if (!req.query.param || typeof(req.query.param) != "string") {
+                res.status(400).send("Invalid parameter to 'setBackgroundColor'");
+                return;    
+            }    
+
+            break;
+        case "setTextScale":
+            if (!req.query.param || typeof(req.query.param) != "string") {
+                res.status(400).send("Invalid parameter to 'setTextScale'");
+                return;    
+            }
+
             break;
         default:
-            break;
+            res.status(400).send("Invalid operation");
+            return;    
     }
-    response.end(JSON.stringify(requestParameters));
-    //response.end(requestURL);
-}
 
-var server = http.createServer(handleHTTPRequest);
-server.listen(5678, function() {console.log("Listener started on port 5678")});
+    io.to(req.query.id).emit(req.query.op, req.query.param);
+})
+
+app.use(function(req, res) {
+    //Redirect other urls to home
+    res.redirect("/");    
+});
+
+io.on("connection", function(socket) {
+    console.log("Socket.io client '" + socket.id + "' connected");
+
+    socket.on("disconnect", function() {
+        console.log("Socket.io client '" + socket.id + "' disconnected");
+    });
+
+    socket.on("set id", function(id) {
+        //De-register from previous channel id
+        console.log("Socket.io client '" + socket.id + "' has registered to channel ID '" + id + "'");
+
+        socket.join(id);
+    });
+});
+
+http.setTimeout(0);
+http.listen(5678, function() {console.log("Listening on port 5678")});
